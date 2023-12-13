72c29ef7fc8b6bf364a6861860f671ee4c153bb3
patanahid <112811361+patanahid@users.noreply.github.com>
2023-12-13 22:34:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from openpyxl.styles import Font
 import os
 import requests
 import json
+import shutil
 
 
 app = Flask(__name__)
@@ -11,25 +12,25 @@ def convert_kruti_to_unicode(input_text):
 
   url = 'https://hindi-font-converter-eight.vercel.app/api/unicode-krutidev'
   headers = {'Content-Type': 'application/json'}
-  
+
   data = {
       'text': input_text,
       'format': 'json',
       'to_font': 'unicode'
   }
-  
+
   response = requests.post(url, headers=headers, data=json.dumps(data))
-  
+
   if response.status_code == 200:
     response_dict = json.loads(json.dumps(response.json()))
-  
+
     # Extracting output_text
     output_text = response_dict.get("data", {}).get("output_text", "")
     print(output_text)
     return output_text
   else:
       return input_text
-      
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -38,7 +39,7 @@ def index():
 def process_file():
     # Assuming your HTML file input has the name 'file'
     uploaded_file = request.files['file']
-    
+    os.makedirs("static")
     # Save the uploaded file
     uploaded_file.save("static/input.xlsx")
 
@@ -79,8 +80,73 @@ def process_file():
     # Remove the in-progress indicator
     os.rename("static/input_in_progress.xlsx", "static/input.xlsx")
 
-    # Provide a download link for the processed file
-    return send_file(output_filename, as_attachment=True, download_name="output.xlsx")
+    try:
+        return send_file(output_filename,
+                        as_attachment=True,
+                        download_name="output.xlsx")
+    finally:
+        global static_folder_path
+        static_folder_path = os.path.join(os.getcwd(), "static")
+        print(static_folder_path)
+        def remove_files_in_static_folder():
+            static_folder_path = os.path.join(os.getcwd(), "static")
+
+        # Check if the folder exists
+        if os.path.exists(static_folder_path) and os.path.isdir(static_folder_path):
+            # List all files in the folder
+            files_in_static = os.listdir(static_folder_path)
+
+            # Iterate through the files and remove them
+            for file_name in files_in_static:
+                file_path = os.path.join(static_folder_path, file_name)
+                try:
+                    if os.path.isfile(file_path):
+                        os.remove(file_path)
+                        print(f"Removed: {file_path}")
+                except Exception as e:
+                    print(f"Error while removing {file_path}: {e}")
+
+                print("All files in the 'static' folder have been removed.")
+            else:
+                print(
+                    "The 'static' folder does not exist in the current working directory."
+                )
+
+        # Run the function
+        remove_files_in_static_folder()
+        shutil.rmtree(os.path.join(os.getcwd(), "static"))
 
 if __name__ == '__main__':
     app.run(debug=False, host="0.0.0.0")
+    def remove_files_in_static_folder():
+        static_folder_path = os.path.join(os.getcwd(), "static")
+
+# Check if the folder exists
+if os.path.exists(static_folder_path) and os.path.isdir(static_folder_path):
+    # List all files in the folder
+    files_in_static = os.listdir(static_folder_path)
+
+    # Iterate through the files and remove them
+    for file_name in files_in_static:
+        file_path = os.path.join(static_folder_path, file_name)
+        try:
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+                print(f"Removed: {file_path}")
+        except Exception as e:
+            print(f"Error while removing {file_path}: {e}")
+
+        print("All files in the 'static' folder have been removed.")
+    else:
+        print(
+            "The 'static' folder does not exist in the current working directory."
+        )
+    try:
+      remove_files_in_static_folder()
+      
+    except Exception as e:
+      print(f"Error while removing {file_path}: {e}")
+    try:
+      shutil.rmtree(os.path.join(os.getcwd(), "static"))
+    except Exception as e:
+      print(f"Shutil Error while removing {file_path}: {e}")
